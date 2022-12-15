4e2625a45b29fe1fcca02c6bcf22596c6c60e3c6
John R. D'Orazio <priest@johnromanodorazio.com>
2022-12-15 23:39:59 +0100

    Create RequestMethod.php

---
+++ includes/enums/RequestMethod.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * enum RequestMethod
+ * Represents all possible Request Methods
+ * for a Request that the API might receive
+ */
+
+class RequestMethod {
+    const POST      = "POST";   //Create / read
+    const GET       = "GET";    //Read
+    const PATCH     = "PATCH";  //Update / modify
+    const PUT       = "PUT";    //Update / replace
+    const DELETE    = "DELETE"; //Delete
+    const OPTIONS   = "OPTIONS";
+    public static array $values = [ "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS" ];
+
+    public static function isValid( $value ) {
+        return in_array( $value, self::$values );
+    }
+}
