fd96f49826eb181019fda0164df7f5f673b27b1f
John R. D'Orazio <priest@johnromanodorazio.com>
2023-01-19 05:40:27 +0100

    test no composer for SampleAPI

--- endpoint.php
+++ endpoint.php
@@ -11,7 +11,7 @@ if( !class_exists("\Composer\Autoload\ClassLoader") ) {
         $prefix = 'JohnRDOrazio\\SimpleAPI\\';
 
         // base directory for the namespace prefix
-        $baseDir = __DIR__.'/src/';
+        $baseDir = __DIR__. DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
 
         // does the class use the namespace prefix?
         $len = strlen($prefix);
@@ -26,7 +26,7 @@ if( !class_exists("\Composer\Autoload\ClassLoader") ) {
         // replace the namespace prefix with the base directory, replace namespace
         // separators with directory separators in the relative class name, append
         // with .php
-        $file = $baseDir.str_replace('\\', '/', $relativeClass).'.php';
+        $file = $baseDir.str_replace('\\', DIRECTORY_SEPARATOR, $relativeClass).'.php';
 
         // if the file exists, require it
         if (file_exists($file)) {
