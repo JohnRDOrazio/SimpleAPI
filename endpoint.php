01786e5fd41d0083aa64db487fd6f3546cb1ebfe
John R. D'Orazio <priest@johnromanodorazio.com>
2023-01-19 05:35:43 +0100

    test no composer for SampleAPI

--- endpoint.php
+++ endpoint.php
@@ -4,8 +4,38 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//if not using composer to autoload the SampleAPI class,
-//uncomment the following line
+if( !class_exists("\Composer\Autoload\ClassLoader") ) {
+    spl_autoload_register(function ($class) {
+
+        // project-specific namespace prefix
+        $prefix = 'JohnRDOrazio\\SimpleAPI\\';
+
+        // base directory for the namespace prefix
+        $baseDir = __DIR__.'/src/';
+
+        // does the class use the namespace prefix?
+        $len = strlen($prefix);
+        if (strncmp($prefix, $class, $len) !== 0) {
+            // no, move to the next registered autoloader
+            return;
+        }
+
+        // get the relative class name
+        $relativeClass = substr($class, $len);
+
+        // replace the namespace prefix with the base directory, replace namespace
+        // separators with directory separators in the relative class name, append
+        // with .php
+        $file = $baseDir.str_replace('\\', '/', $relativeClass).'.php';
+
+        // if the file exists, require it
+        if (file_exists($file)) {
+            require $file;
+        }
+    });
+
+}
+
 include_once( "SampleAPI.php" );
 
 $SampleAPI = new SampleAPI();
